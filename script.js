fa60cf98dce78f4a9ae6f59bfdb94a5bdedbc3de
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-13 15:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+let btn = document.querySelector("input");
+btn.addEventListener("click",removeData);
+function removeData(){
+	let list = document.getElementById("colorSelect");
+	list.remove(list.selectedIndex);
+
+}
